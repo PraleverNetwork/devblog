10724f01e70ee4d0ced2910068c3c7343ad9f8ef
advn1357 <advn1357@outlook.kr>
2024-07-16 12:53:39 +0900

    구조 변경

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 
 
 //boot
-console.log(window.location.search)
+// console.log(window.location.search)
 var params = new URLSearchParams(window.location.search)
 
-if(params.has("id")){
-    loadData(params.get("id"));
+// window.addEventListener('popstate', function (event) {
+//     getLoadingData();
+// });
+
+getLoadingData();
+
+function getLoadingData(){
+    if(params.has("id")){
+        loadData(params.get("id"));
+    }
+    else{
+        loadData("home");
+    }
+    listUpdate();
 }
-else{
-    loadData("home");
-}
-listUpdate();
 
 function loadData(url){
     var http = new XMLHttpRequest();
@@ -33,6 +41,7 @@ function loadData(url){
 
         //body.innerHTML = response
         document.getElementById("source").innerHTML = response;
+        // history.pushState(null, document.title, "/?id="+url);
         document.title = "LandWar DevLog - "+document.getElementById("header").innerText;
         summonShareLink(url);
     }
@@ -65,12 +74,40 @@ function listUpdate(){
 
         var json = JSON.parse(response);
         
-        json.forEach(item => {
-            console.log("[PRLDevBlog] Collected item: "+item.id);
-            var date = new Date(item.date);
-            var dateString = date.toLocaleString();
-            result += '<div class="item" onclick="menuHide();loadData(\''+item.id+'\')"><h3>'+item.title+'</h3><div>'+dateString+'</div></div>';
+        json.forEach(root => {
+            if(root.root != null){
+                console.log("[PRLDevBlog] Collected Root: "+root.root);
+                var rootTitle = root.root;
+
+                var items = ""
+                root.datas.forEach(item => {
+                    console.log("[PRLDevBlog] Collected "+rootTitle+" Item: "+item.id);
+                    var title = item.id
+                    var date = new Date(item.date);
+                    var dateString = date.toLocaleString();
+                    items += '<div class="item" onclick="menuHide();loadData(\''+item.id+'\')"><h3>'+title+'</h3><div>'+dateString+'</div></div>';
+                });
+
+                result += '<div class="root">'
+                + '<h3  onclick="rootItemDisplay(\''+root.root+'\')"><span id="'+root.root+'-direction">></span> '+root.root+'</h3>'
+                + '<div class="rootitem" id="'+root.root+'-items" style="display: none;">' + items + '</div></div>'
+            }
+            else if (root.id != null){
+                    console.log("[PRLDevBlog] Collected Top item: "+root.id);
+                    var date = new Date(root.date);
+                    //TODO 임시
+                    var title = root.id
+                    var dateString = date.toLocaleString();
+                    result += '<div class="item" onclick="menuHide();loadData(\''+root.id+'\')"><h3>'+title+'</h3><div>'+dateString+'</div></div>';
+            }
         });
+
+        // json.forEach(item => {
+        //     console.log("[PRLDevBlog] Collected item: "+item.id);
+        //     var date = new Date(item.date);
+        //     var dateString = date.toLocaleString();
+        //     result += '<div class="item" onclick="menuHide();loadData(\''+item.id+'\')"><h3>'+item.title+'</h3><div>'+dateString+'</div></div>';
+        // });
 
         document.getElementById("list").innerHTML = result;
         console.log("[PRLDevBlog] Finish collecting data");
@@ -103,4 +140,18 @@ function menuHide(){
         document.getElementById("menu").style.display = "none";
     }
     document.getElementById("background").style.display = "none";
+}
+
+function rootItemDisplay(id){
+    var direction = document.getElementById(id+'-direction')
+    var item = document.getElementById(id+'-items');
+
+    if(item.style.display == "none"){
+        item.style.display = "block"
+        direction.innerHTML = "–"
+    }
+    else{
+        item.style.display = "none";
+        direction.innerHTML = ">"
+    }
 }
